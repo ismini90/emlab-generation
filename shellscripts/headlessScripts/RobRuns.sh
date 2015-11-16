4674d0ccc18ac46099e3d720357b496329470633
Kaveri3012 <kaveri.kariappa.i@gmail.com>
2015-11-16 13:54:38 +0100

    made scripts for Rob's runs

---
+++ shellscripts/headlessScripts/RobRuns.sh
@@ -0,0 +1,9 @@
+./scenarioRuns.sh RDJ-RT-TC-ThreeMultiNodes-BaseCase RDJ-RT-TC-ThreeMultiNodes-BaseCase 1 1 120 0m 10:00:00
+./scenarioRuns.sh RDJ-RT-TC-ThreeMultiNodes-InfiniteCap RDJ-RT-TC-ThreeMultiNodes-InfiniteCap 1 1 120 0m 10:00:00
+./scenarioRuns.sh RDJ-RT-TC-ThreeMultiNodes-PacedTarget RDJ-RT-TC-ThreeMultiNodes-PacedTarget 1 1 120 0m 10:00:00
+./scenarioRuns.sh RDJ-RT-TC-ThreeMultiNodes-TechSpec RDJ-RT-TC-ThreeMultiNodes-TechSpec 1 1 120 0m 10:00:00
+./scenarioRuns.sh FuturePoint2 FuturePoint2 1 1 120 0m 10:00:00
+./scenarioRuns.sh FuturePoint3 FuturePoint3 1 1 120 0m 10:00:00
+./scenarioRuns.sh FuturePoint4 FuturePoint4 1 1 120 0m 10:00:00
+./scenarioRuns.sh FuturePoint5 FuturePoint5 1 1 120 0m 10:00:00
+RDJ-RT-TC-ThreeMultiNodes-BaseCase
